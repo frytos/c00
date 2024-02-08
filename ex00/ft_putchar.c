6e03e2b820e607113ae92f384be4ff873cf4e740
Fryt Os <frytos@Fryts-Mac-mini.local>
2024-02-08 16:41:22 +0100

    corrects typo to be able to compile

--- ex00/ft_putchar.c
+++ ex00/ft_putchar.c
@@ -6,14 +6,21 @@
 /*   By: argrouss <argrouss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 14:21:49 by argrouss          #+#    #+#             */
-/*   Updated: 2024/02/08 14:35:38 by argrouss         ###   ########.fr       */
+/*   Updated: 2024/02/08 16:24:18 by argrouss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include <unistd.h>
 
 void	ft_putchar(char c);
 
-void	fr_putchar(char c)
+void	ft_putchar(char c)
 {
 	write(1, &c, 1);
+}
+
+int	main(void)
+{
+	ft_putchar(100);
+	return (0);
 }
