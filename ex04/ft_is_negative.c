566f7f712edade9bf20b6aa3abfe2185b7c5e0b0
Fryt Os <frytos@Fryts-Mac-mini.local>
2024-02-08 19:26:01 +0100

    adds the ft is negative function

---
+++ ex04/ft_is_negative.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_negative.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: argrouss <argrouss@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/08 18:18:33 by argrouss          #+#    #+#             */
+/*   Updated: 2024/02/08 19:21:21 by argrouss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_is_negative(int n);
+
+int	main(void)
+{
+	ft_is_negative(-3);
+	ft_is_negative(0);
+	ft_is_negative(7);
+	return (0);
+}
+
+void	ft_is_negative(int n)
+{
+	int	pos;
+	int	neg;
+
+	pos = 80;
+	neg = 78;
+	if (n >= 0)
+	{
+		write(1, &pos, 1);
+	}
+	else
+	{
+		write(1, &neg, 1);
+	}
+}
