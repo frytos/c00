b2b80681bdc291af2aa368cd2c97e4e7c575fdce
Fryt Os <frytos@Fryts-Mac-mini.local>
2024-02-08 17:50:13 +0100

    adds the reverse print with while only

---
+++ ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: argrouss <argrouss@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/08 16:40:06 by argrouss          #+#    #+#             */
+/*   Updated: 2024/02/08 17:33:02 by argrouss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void);
+
+int	main(void)
+{
+	ft_print_reverse_alphabet();
+	return (0);
+}
+
+void	ft_print_reverse_alphabet(void)
+{
+	int	c;
+
+	c = 122;
+	while (c > 96)
+	{
+		write(1, &c, 1);
+		c--;
+	}
+}
