abd6db244d7cdf4fe10831695d01df30e0664d42
Fryt Os <frytos@Fryts-Mac-mini.local>
2024-02-09 01:40:14 +0100

    final checks on v4 of comb function

--- ex05/ft_print_comb4.c
+++ ex05/ft_print_comb4.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*   ft_print_comb4.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: argrouss <argrouss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 19:29:37 by argrouss          #+#    #+#             */
-/*   Updated: 2024/02/08 23:46:08 by argrouss         ###   ########.fr       */
+/*   Updated: 2024/02/09 01:38:43 by argrouss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include <stdio.h>
 
 void	ft_print_comb(void);
-void	check_ascending_order_and_print(char *nb_to_print);
+void	check_to_print(char *nb_to_check, int nb_to_check_size);
 
 int	main(void)
 {
@@ -24,9 +24,14 @@ int	main(void)
 
 void	ft_print_comb(void)
 {
-	char	nb_f[4];
-	
+	char	nb_f[6];
+	int		nb_f_size;
+
 	nb_f[0] = 48;
+	nb_f[3] = 44;
+	nb_f[4] = 32;
+	nb_f[5] = '\0';
+	nb_f_size = 6;
 	while (nb_f[0] < 58)
 	{
 		nb_f[1] = 48;
@@ -35,8 +40,7 @@ void	ft_print_comb(void)
 			nb_f[2] = 48;
 			while (nb_f[2] < 58)
 			{
-				ıwrite(1, &nb_f, 5);
-				check_ascending_order_and_print(nb_f);
+				check_to_print(nb_f, nb_f_size);
 				nb_f[2]++;
 			}
 			nb_f[1]++;
@@ -45,17 +49,17 @@ void	ft_print_comb(void)
 	}
 }
 
-void	check_ascending_order_and_print(char *nb_to_print)
+void	check_to_print(char *nb_to_check, int nb_to_check_size)
 {
-	if (nb_to_print[0] < nb_to_print[1] && (nb_to_print[1] < nb_to_print[2]))
+	if (nb_to_check[0] < nb_to_check[1] && (nb_to_check[1] < nb_to_check[2]))
 	{
-		nb_f[3] = 44;
-		nb_f[4] = 0;	
-		write(1, &nb_to_print, 5);
-		printf("\n");
-		if (nb_to_print[0] == 55 &&  (nb_to_print[1] == 56 &&  nb_to_print[2] == 57))
+		write(1, nb_to_check, nb_to_check_size);
+		if (nb_to_check[0] == 55 && nb_to_check[1] == 56)
 		{
-			//write(1, &nb_to_print, 3);
+			if (nb_to_check[2] == 57)
+			{
+				write(1, nb_to_check, nb_to_check_size - 3);
+			}
 		}
 	}
 }
